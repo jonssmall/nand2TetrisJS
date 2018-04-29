655c2cd32658e62579b3ae9c1bca44dcb4f30803
Jon Small <jonssmall@gmail.com>
2018-04-29 15:32:26 -0500

    full adder chip with test

--- js/ch2/chips.js
+++ js/ch2/chips.js
@@ -7,6 +7,16 @@ function halfAdder(a, b) {
   };
 }
 
+
+function fullAdder(a, b, c) {
+  const half1 = halfAdder(a,b);
+  const half2 = halfAdder(c, half1.sum);
+  return {
+    sum: half2.sum,
+    carry: gates.OR(half1.carry, half2.carry)
+  };
+};
+
 module.exports = {
-  halfAdder
+  halfAdder, fullAdder
 };
--- spec/ch2/chips.spec.js
+++ spec/ch2/chips.spec.js
@@ -1,6 +1,7 @@
 const chips = require("../../js/ch2/chips");
 
 describe("Half-Adder Chip", halfAdderTest());
+describe("Full-Adder Chip", fullAdderTest());
 
 function halfAdderTest() {
   const truth = [
@@ -13,6 +14,26 @@ function halfAdderTest() {
     truth.forEach(row => {
       it(`${JSON.stringify(row)}`, () => {
         expect(chips.halfAdder(row.a, row.b)).toEqual({ carry: Boolean(row.carry), sum: Boolean(row.sum) });
+      });
+    });
+  }
+}
+
+function fullAdderTest() {
+  const truth = [
+    { a: 0, b: 0, c: 0, carry: 0, sum: 0 },
+    { a: 0, b: 0, c: 1, carry: 0, sum: 1 },
+    { a: 0, b: 1, c: 0, carry: 0, sum: 1 },
+    { a: 0, b: 1, c: 1, carry: 1, sum: 0 },
+    { a: 1, b: 0, c: 0, carry: 0, sum: 1 },
+    { a: 1, b: 0, c: 1, carry: 1, sum: 0 },
+    { a: 1, b: 1, c: 0, carry: 1, sum: 0 },
+    { a: 1, b: 1, c: 1, carry: 1, sum: 1 }
+  ];
+  return () => {
+    truth.forEach(row => {
+      it(`${JSON.stringify(row)}`, () => {
+        expect(chips.fullAdder(row.a, row.b, row.c)).toEqual({ carry: Boolean(row.carry), sum: Boolean(row.sum) });
       });
     });
   }
