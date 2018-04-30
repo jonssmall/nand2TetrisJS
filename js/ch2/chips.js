341f5eb0f712d1bb5824894c03bd7ad62eb65462
Jon Small <jonssmall@gmail.com>
2018-04-29 21:24:43 -0500

    adder16 with test coverage. might revisit choice of data type I/O

--- js/ch2/chips.js
+++ js/ch2/chips.js
@@ -1,12 +1,13 @@
 const gates = require("../ch1/gates");
 
+// half-adder, full-adder, add16, inc16, ALU
+
 function halfAdder(a, b) {
   return {
     sum: gates.XOR(a,b),
     carry: gates.AND(a,b)
   };
 }
-
 
 function fullAdder(a, b, c) {
   const half1 = halfAdder(a,b);
@@ -17,6 +18,37 @@ function fullAdder(a, b, c) {
   };
 };
 
+// n-bit adder, used for 16 bits
+// assumes each input bus is same length
+// returns binary number in left-to-right, index is nth bit
+function adderN(arrA, arrB) {
+  const first = fullAdder(arrA[0], arrB[0], 0);
+  return arrA.slice(1).reduce((acc, e, i, a) => {
+    return [...acc, fullAdder(arrA[i+1], arrB[i+1], acc[i].carry)];
+  }, [first]); 
+}
+
+// non-array input
+function ezAdder(inA, inB) {
+  return adderN(binaryToArray(inA), binaryToArray(inB));
+}
+
+// in: "11010", out: [0,1,0,1,1]
+function binaryToArray(binaryString) {
+  return Array.from(binaryString).map(Number).reverse();
+}
+
+// receives and returns 16-bit binary in human-readable form e.g. 001 + 101 = 110
+function adder16(inA, inB) {
+  if(inA.length === 16 && inB.length === 16) {
+    return ezAdder(inA, inB).reduce((acc, e) => { // e = {sum: false, carry:false}
+      return [...acc, e.sum ? 1 : 0];
+    }, []).reverse().join("");
+  } else {
+    throw new Error("Both input buses must be 16 bits");
+  }
+}
+
 module.exports = {
-  halfAdder, fullAdder
+  halfAdder, fullAdder, adder16
 };
--- spec/ch2/chips.spec.js
+++ spec/ch2/chips.spec.js
@@ -2,6 +2,7 @@ const chips = require("../../js/ch2/chips");
 
 describe("Half-Adder Chip", halfAdderTest());
 describe("Full-Adder Chip", fullAdderTest());
+describe("16-bit Adder Chip", adder16Test());
 
 function halfAdderTest() {
   const truth = [
@@ -35,6 +36,25 @@ function fullAdderTest() {
       it(`${JSON.stringify(row)}`, () => {
         expect(chips.fullAdder(row.a, row.b, row.c)).toEqual({ carry: Boolean(row.carry), sum: Boolean(row.sum) });
       });
+    });
+  }
+}
+
+function adder16Test() {
+  return () => {
+    it("Must have a 16 bit bus for each input", () => {
+      expect(function() { chips.adder16([1,0,0], [0,0,0]) }).toThrowError("Both input buses must be 16 bits")
+    });
+    it("Returns binary as a string", () => {
+      const inA = "0000000000000000";
+      const inB = "0000000000000001";
+      expect(chips.adder16(inA, inB)).toBe("0000000000000001");
+      const inA2 = "1111111111111111";      
+      expect(chips.adder16(inA2, inB)).toBe("0000000000000000");
+      const inA3 = "1000101010111001";
+      const inB3 = "0001110101011111";
+      const res3 = "1010100000011000";
+      expect(chips.adder16(inA3, inB3)).toBe(res3);
     });
   }
 }
